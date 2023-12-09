ce60daf5d70f41d65e830e4a75e6897fe1127433
wellpereiradevs <132404088+wellpereiradevs@users.noreply.github.com>
2023-12-09 18:26:47 -0300

    Project Created

---
+++ src/main/java/com/wellpereiradevs/springbootwebjpa/SpringbootWebJpaApplication.java
@@ -0,0 +1,13 @@
+package com.wellpereiradevs.springbootwebjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wellpereiradevs/springbootwebjpa/SpringbootWebJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellpereiradevs.springbootwebjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
