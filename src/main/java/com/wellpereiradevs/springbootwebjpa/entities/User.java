b429488db0156271f8ba40c264118e2bdeef9932
wellpereiradevs <132404088+wellpereiradevs@users.noreply.github.com>
2023-12-10 18:14:21 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/wellpereiradevs/springbootwebjpa/entities/User.java
+++ src/main/java/com/wellpereiradevs/springbootwebjpa/entities/User.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "tb_user")
@@ -19,6 +22,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {}
 
@@ -68,5 +74,24 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
