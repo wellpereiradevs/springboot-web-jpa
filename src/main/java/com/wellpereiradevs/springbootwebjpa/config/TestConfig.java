3c5644387946ac9a358d50ec4e2a119692368eca
wellpereiradevs <132404088+wellpereiradevs@users.noreply.github.com>
2023-12-10 21:20:10 -0300

    OrderStatus enum

--- src/main/java/com/wellpereiradevs/springbootwebjpa/config/TestConfig.java
+++ src/main/java/com/wellpereiradevs/springbootwebjpa/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.wellpereiradevs.springbootwebjpa.config;
 
 import com.wellpereiradevs.springbootwebjpa.entities.Order;
 import com.wellpereiradevs.springbootwebjpa.entities.User;
+import com.wellpereiradevs.springbootwebjpa.entities.enums.OrderStatus;
 import com.wellpereiradevs.springbootwebjpa.repositories.OrderRepository;
 import com.wellpereiradevs.springbootwebjpa.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
         User user1 = new User(null, "Maria Brown", "maria@gmail.com", "98888-8888", "123456");
         User user2 = new User(null, "Alex Green", "alex@gmail.com", "97777-7777", "123456");
 
-        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1);
-        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2);
-        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user1);
+        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID ,user1);
+        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2);
+        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT ,user1);
 
         userRepository.saveAll(Arrays.asList(user1, user2));
         orderRepository.saveAll(Arrays.asList(order1, order2, order3));
--- src/main/java/com/wellpereiradevs/springbootwebjpa/entities/Order.java
+++ src/main/java/com/wellpereiradevs/springbootwebjpa/entities/Order.java
@@ -1,6 +1,7 @@
 package com.wellpereiradevs.springbootwebjpa.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.wellpereiradevs.springbootwebjpa.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -22,15 +23,19 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private OrderStatus orderStatus ;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
-    public Order() {}
+    public Order() {
+    }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -48,6 +53,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(String.valueOf(orderStatus));
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus;
+        }
     }
 
     public User getClient() {
