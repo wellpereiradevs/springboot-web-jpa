f919dbbfd4422fd7a5d142ddbd21d941440f5fcc
wellpereiradevs <132404088+wellpereiradevs@users.noreply.github.com>
2024-01-09 11:51:54 -0300

    ManyToMany association with JoinTable

--- src/main/java/com/wellpereiradevs/springbootwebjpa/config/TestConfig.java
+++ src/main/java/com/wellpereiradevs/springbootwebjpa/config/TestConfig.java
@@ -49,6 +49,14 @@ public class TestConfig implements CommandLineRunner {
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
         User user1 = new User(null, "Maria Brown", "maria@gmail.com", "98888-8888", "123456");
         User user2 = new User(null, "Alex Green", "alex@gmail.com", "97777-7777", "123456");
 
